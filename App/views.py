46ed26ba1470735709e1a5b5de7fe99ff8d4e748
Kamil Kowalczyk <PrimeLaft@gmail.com>
2016-01-08 23:16:27 +0100

    Support for libs

--- App/views.py
+++ App/views.py
@@ -1,26 +1,16 @@
 from django.shortcuts import render_to_response,redirect
 from django.template import RequestContext
 from App.models import UserData
-
+from Libs.User import UserLib
 
 def UserEdit(request):
 
+    UserLib.UserEdit(request)
 
-    count = UserData.objects.filter(Username = "Kamilek").count()
-
-    if count is 0:
-        data = UserData()
-        data.Username = "Kamilek"
-        data.Sex = "Man"
-        data.SexWant = "Woman"
-        data.Description = "Jestem Kamil i szukam ciekawych znajomosci"
-        data.Location = "Kielce"
-        data.Photos = "myphoto.png, phoo.jpg, selfie.jpg"
-        data.Name = "Kamil"
-        data.RadiusWant = "100"
-        data.save()
-
-    newvar = UserData.objects.get(Username = "Kamilek")
+    UserLib.SetUserData(request, {'Username': 'Pati', 'Sex': 'Woman', 'SexWant': 'Man'})
 
 
-    return render_to_response('home.html', { 'Name': newvar.Name, 'Location': newvar.Location }, context_instance=RequestContext(request))
+    newvar = UserData.objects.get(Username = "Pati")
+
+
+    return render_to_response('home.html', { 'Name': newvar.Username, 'Location': newvar.Location }, context_instance=RequestContext(request))
---
+++ Libs/User.py
@@ -0,0 +1,34 @@
+from App.models import UserData
+
+
+class UserLib:
+
+    # This is description for this function. Thats very very long
+    # use:
+    # You can use that for create or edit user
+
+    def UserEdit(self):
+        print('\033[93m' + 'This is the first library!');
+        return 1
+
+
+
+
+    # This is description for this function. Thats very very long
+    # use:
+    # You can use that for create or edit user
+
+    def GetUserData(self):
+        return 1
+
+
+
+
+    # This is description for this function. Thats very very long
+    # use:
+    # You can use that for create or edit user
+
+    def SetUserData(self, data):
+        UserData.objects.filter(Username="Patrycja").update(**data)
+        return 1
+
